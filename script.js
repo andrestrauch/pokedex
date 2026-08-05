0e2a9f950b9c51363fd1760b51e245ee2196e87b
Andre Strauch <andrestrauch@gmx.de>
2026-08-05 14:56:17 +0200

    add render types

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function init() {
     getTypes();
     setTimeout(() => {
         getData();
-    }, 500);
+    }, 900);
 }
 
 function renderPkmn(pokemonList) {
@@ -19,15 +19,26 @@ function renderPkmn(pokemonList) {
                 <div class="pkmn-img" id="pkmnColor${i}">
                     <img src="${pokemonList[i].img}" alt="">
                 </div>
-                <div class="pkmn-types">
-                    <img src="${pokemonList[i].imgT1}" alt="">
-                    <img src="${pokemonList[i].imgT2}" alt="">
+                <div class="pkmn-types" id="pkmnTypes${i}">
                 </div>
             <div>
         `;
         setBgColor(i);
+        renderTypes(i, pokemonList);
     }
     renderLoadBtn();
+}
+function renderTypes(i, pokemonList) {
+    const typeRef = document.getElementById(`pkmnTypes${i}`);
+    typeRef.innerHTML = "";
+    typeRef.innerHTML += /*html*/ `
+        <img src="${pokemonList[i].imgT1}" alt="">
+    `;
+    if (pokemonList[i].imgT2 != "") {
+        typeRef.innerHTML += /*html*/ `
+            <img src="${pokemonList[i].imgT2}" alt="">
+        `;
+    }
 }
 
 function renderLoadBtn() {
@@ -50,6 +61,16 @@ function setBgColor(i) {
     colorRef.classList.add(`bg-${POKEMON[i].typ1}`);
 }
 
+async function getTypes() {
+    for (let t = 0; t < 18; t++) {
+        let typ = await fetch(`https://pokeapi.co/api/v2/type/${t + 1}`);
+        let typFromJSN = await typ.json();
+        TYPES[t] =
+            typFromJSN.sprites["generation-viii"]["sword-shield"].symbol_icon;
+    }
+    // console.log(TYPES[11]);
+}
+
 async function getData() {
     for (let k = forStart; k < dataCount; k++) {
         if (dataStart == 0) dataStart = 1;
@@ -67,14 +88,28 @@ async function getData() {
     }, 1000);
 }
 
-async function getTypes() {
-    for (let t = 0; t < 18; t++) {
-        let typ = await fetch(`https://pokeapi.co/api/v2/type/${t + 1}`);
-        let typFromJSN = await typ.json();
-        TYPES[t] =
-            typFromJSN.sprites["generation-viii"]["sword-shield"].symbol_icon;
-    }
-    // console.log(TYPES[11]);
+function setType(typing) {
+    let type = "";
+    if (typing == "normal") type = TYPES[0];
+    else if (typing == "fighting") type = TYPES[1];
+    else if (typing == "flying") type = TYPES[2];
+    else if (typing == "poison") type = TYPES[3];
+    else if (typing == "ground") type = TYPES[4];
+    else if (typing == "rock") type = TYPES[5];
+    else if (typing == "bug") type = TYPES[6];
+    else if (typing == "ghost") type = TYPES[7];
+    else if (typing == "steel") type = TYPES[8];
+    else if (typing == "fire") type = TYPES[9];
+    else if (typing == "water") type = TYPES[10];
+    else if (typing == "grass") type = TYPES[11];
+    else if (typing == "electric") type = TYPES[12];
+    else if (typing == "psychic") type = TYPES[13];
+    else if (typing == "ice") type = TYPES[14];
+    else if (typing == "dragon") type = TYPES[15];
+    else if (typing == "dark") type = TYPES[16];
+    else if (typing == "fairy") type = TYPES[17];
+    else type = "";
+    return type;
 }
 
 function setStats(responseFromJSON) {
@@ -87,6 +122,7 @@ function setStats(responseFromJSON) {
         obj.typ2 = responseFromJSON.types[1].type.name;
     else obj.typ2 = "";
     obj.imgT1 = setType(obj.typ1);
+    // console.log(obj.imgT1);
     if (obj.typ2 != "") obj.imgT2 = setType(obj.typ2);
     else obj.imgT2 = "";
 
@@ -107,31 +143,6 @@ function setStats(responseFromJSON) {
     // console.log(obj);
     POKEMON.push(obj);
     PKMN = POKEMON;
-}
-
-function setType(typing) {
-    let type;
-    if (typing == "normal") type = TYPES[0];
-    else if (typing == "fighting") type = TYPES[1];
-    else if (typing == "flying") type = TYPES[2];
-    else if (typing == "poison") type = TYPES[3];
-    else if (typing == "ground") type = TYPES[4];
-    else if (typing == "rock") type = TYPES[5];
-    else if (typing == "bug") type = TYPES[6];
-    else if (typing == "ghost") type = TYPES[7];
-    else if (typing == "steel") type = TYPES[8];
-    else if (typing == "fire") type = TYPES[9];
-    else if (typing == "water") type = TYPES[10];
-    else if (typing == "grass") type = TYPES[11];
-    else if (typing == "electric") type = TYPES[12];
-    else if (typing == "psychic") type = TYPES[13];
-    else if (typing == "ice") type = TYPES[14];
-    else if (typing == "dragon") type = TYPES[15];
-    else if (typing == "dark") type = TYPES[16];
-    else if (typing == "fairy") type = TYPES[17];
-    else type = "";
-
-    return type;
 }
 
 function loadMore() {
